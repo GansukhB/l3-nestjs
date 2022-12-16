7ee5e36bdc2fb8d7d0d010921d13c42cb4c26449
Gansukh Batjargal <gansukhb@Gansukhs-MacBook-Pro.local>
2022-12-16 10:36:41 +0800

    Sample middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,19 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { forwardRef, MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
 import { DatabaseModule } from './database/database.module';
 import { ContentsController } from './contents/contents.controller';
 import { ContentsModule } from './contents/contents.module';
+import { SampleMiddleware } from './sample.middleware';
 
 @Module({
   imports: [BooksModule, DatabaseModule, ContentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(SampleMiddleware).forRoutes('contents');
+  }
+}
---
+++ src/sample.middleware.ts
@@ -0,0 +1,11 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class SampleMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    console.log('Request...');
+    res.send('Forbidden');
+    //next();
+  }
+}
